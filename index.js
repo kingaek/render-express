55634b4d2bc6eaf629a7cecbc4d7c57e806d7413
aekryz1993 <ryzaek1993@gmail.com>
2022-09-14 16:25:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => {
+  res.cookie("token", "value");
+  res.send("Hello Render, Can I setup cookies please");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
