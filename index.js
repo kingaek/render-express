5099ff7fab32c9f5fcbcd2f871f7ccc00310c379
aekryz1993 <ryzaek1993@gmail.com>
2022-09-15 09:40:45 +0100

    fix cookie

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
 
+const whitelist = [
+  "http://localhost:5173",
+  "https://6322e3af8900897ddea8d571--glistening-klepon-c03030.netlify.app",
+];
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
-  res.cookie("token", "value");
+  res.cookie("token", "value", { sameSite: "none", secure: true });
   res.json({ msg: "Hello Render, Can I setup cookies please" });
 });
 
